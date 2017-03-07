662ec06d00ed0e3aa1063786ab14b86be1684dd9
beiquan021 <yangjingjing>
2017-03-07 14:40:16 +0800

    main.index angular多层循环

--- url.js
+++ url.js
@@ -5,12 +5,14 @@ http.createServer(function (req, res) {    //创建server(req,res)
   // res.writeHead(200, {'Content-Type': 'tetx/html; charset=utf-8'})   //第一个参数200，状态码，第二个参数，传入的是html
   if(req.url !== 'favicon.con') {
     // console.log(req.url);
-    var path = req.url.replace(/\//, '')    //将路由的'/',去掉
+    // var path = req.url.replace(/\//, '')    //将路由的'/',去掉
+    var path = req.url.split('?')[0].replace(/\//, '')    //将路由的'/',去掉
+
     console.log(path);
     try {                               //先try一个function，如果try失败，就执行catch方法
-      router[path](res)     //实现路由
+      router[path](res, req)     //实现路由
     } catch(e) {
-      router['home'](res)     //设置了默认路由
+      router['home'](res, req)     //设置了默认路由
     }
     // router.home(res)
     // res.end()      //在设置路由router.js的readFile()报错，end()多了
--- views/router.js
+++ views/router.js
@@ -1,4 +1,6 @@
 var files = require('./files.js')
+var url = require('url')
+var queryString = require('querystring')
 
 module.exports = {       //commonjs把exports、 modile exports都暴露了
   home: function (res) {
@@ -7,15 +9,35 @@ module.exports = {       //commonjs把exports、 modile exports都暴露了
     // res.write('首页')
     // res.end()
   },
-  login: function (res) {
+  login: function (res, req) {
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})   //第一个参数200，状态码，第二个参数，传入的是html
+    //1.get请求
+    // var urlObject = url.parse(req.url, true).query
+    // if(urlObject.email && urlObject.password) {
+    //   console.log(urlObject)
+    //   console.log(urlObject.password)
+    // }
+
+    //2.post请求
+    var post = ''
+    req.on('data', function (chunk) {
+      post += chunk    //将信息串起来
+    })
+    req.on('end', function () {
+      var urlObject = queryString.parse(post)
+      console.log(urlObject.email)
+    })
+
     files.readFile('./templates/login.html', res)    //file.js的readFile已经有下面的两行执行代码，所以直接就去掉了
 
   },
   register: function (res) {
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})   //第一个参数200，状态码，第二个参数，传入的是html
     files.readFile('./templates/register.html', res)    //file.js的readFile已经有下面的两行执行代码，所以直接就去掉了
-
+  },
+  angular: function (res) {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})   //第一个参数200，状态码，第二个参数，传入的是html
+    files.readFile('./templates/angular.html', res)    //file.js的readFile已经有下面的两行执行代码，所以直接就去掉了
   },
   image: function (res) {
     res.writeHead(200, {'Content-Type': 'image/jpeg'})   //第一个参数200，状态码，第二个参数，传入的是html
@@ -24,9 +46,22 @@ module.exports = {       //commonjs把exports、 modile exports都暴露了
 }
 
 
-  //  单独用exports暴露的话,就得写成exports.router_name = function() {} 的形式
+  // 1. 单独用exports暴露的话,就得写成exports.router_name = function() {} 的形式
 
 // exports.home = function (res) {
 //   res.write('首页')
 //   res.end()
 // }
+//2.  url
+// login
+// { parse: [Function: urlParse],
+//   resolve: [Function: urlResolve],
+//   resolveObject: [Function: urlResolveObject],
+//   format: [Function: urlFormat],
+//   URL:
+//    { [Function: URL]
+//      originFor: [Function],
+//      domainToASCII: [Function],
+//      domainToUnicode: [Function] },
+//   Url: [Function: Url] }
+// favicon.ico
